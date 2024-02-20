d94b7966e4b53251308f8f930b7ed2ed50b57d11
Pierre COURBIN <pierre.courbin@gmail.com>
2024-02-20 18:36:04 +0100

    Update example to change premption only on one specific phase of one task

--- test.py
+++ test.py
@@ -5,18 +5,19 @@ from schedule_display import ScheduleDisplay
 
 datajson = {
     "tasks": [
-        {"Name": "T1", "O": 0, "R": 2, "E": 4, "W": 1, "D": 10, "T": 10},
         {
-            "Name": "T2",
-            "O": 1,
+            "Name": "T1",
+            "O": 0,
             "Phases": [
-                {"Type": "Memory", "Duration": 1, "Premption": True},
-                {"Type": "Processor", "Duration": 1, "Premption": True},
+                {"Type": "Memory", "Duration": 2, "Premption": False},
+                {"Type": "Processor", "Duration": 4, "Premption": True},
                 {"Type": "Memory", "Duration": 1, "Premption": True},
             ],
-            "D": 4,
-            "T": 9,
+            "D": 10,
+            "T": 10,
         },
+
+        {"Name": "T2", "O": 1, "R": 1, "E": 1, "W": 1, "D": 4, "T": 9},
         {"Name": "T3", "O": 0, "C": 2, "D": 5, "T": 6},
     ],
     "resources": [
@@ -35,7 +36,7 @@ max_time = 40
 scheduler = Scheduler(tasks, resources, premption_processor=True, premption_memory=True)
 scheduler.schedule(max_time)
 schedule = scheduler.schedule_result
-print(schedule)
+#print(schedule)
 
 graph = ScheduleDisplay(max_time=max_time, render="browser")
 graph.update(
